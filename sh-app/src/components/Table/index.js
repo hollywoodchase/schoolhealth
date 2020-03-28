e0c5910c3e8fb386ff9cc39a63b664b55458b5cd
Danny Calise <dannyjcalise@gmail.com>
2020-03-28 10:15:36 -0400

    ready to apply database commands

--- sh-app/src/components/Table/index.js
+++ sh-app/src/components/Table/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 // import API from "../../utils/API";
+import axios from 'axios';
 import "../../style.css";
 
 // class Table extends React.Component {
@@ -18,60 +19,98 @@ import "../../style.css";
 //   }
 
 class Table extends Component {
-    state = {
-        client: [],
-        quantity: "",
-        price: "",
-        inCart: ""
-      };
+    constructor(props) {
+        super(props);
+        this.state = {
+            client: "",
+            quantity: "",
+            price: "",
+            inCart: ""
+        };
+        // this.handleClick = this.handleClick.bind(this);
+        this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+
+
 
     increaseQuantity() {
-        // e.preventDefault();
-
-        // console.log('The quantity is now ' + this.quantity);
-        // console.log('The price is now ' + this.price);
-        // console.log('The current price is ' + (this.price * this.quantity));
+        // this.setState((client, quantity, price, inCart) => {
+        //     return {};
+        // });
+        // this.setState((state) => ({ client: "CLIENT", quantity: state.quantity + 1, price: this.childNode, inCart: 0 }));
         console.log('This is ', this);
+        console.log('Friend');
     };
+
+    // printInfo = () => {
+    //     axios.get('/api/products', {
+    //         params: {
+    //           ID: 12345
+    //         }
+    //       }
+    //         .then(res => {
+    //             this.setState({ info: [] });
+
+    //             const productInfo = [];
+    //             for (let i = 0; i < res.data.length; i++) {
+    //                 console.log("FOOL " + JSON.stringify(res.data[0]))
+    //                 jobInfo.push({
+    //                     id: res.data[i]._id,
+    //                     title: res.data[i].title,
+    //                     description: res.data[i].description,
+    //                     educationNeeded: res.data[i].educationNeeded,
+    //                     salary: res.data[i].medianSalary,
+    //                     jobsAvailable: res.data[i].projectedJobs,
+    //                     link: res.data[i].link,
+    //                     hourly: res.data[i].hourlyWage,
+    //                     rent: res.data[i].rent,
+    //                     image: res.data[i].image,
+    //                     selected: false
+    //                 })
+    //             }
+    //             this.setState({ info: jobInfo })
+    //         });
+    // }
 
     render() {
         return (
-                <div className="table">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Item</th>
-                                <th scope="col" className="quantity">Quantity</th>
-                                <th scope="col" className="price">Price</th>
-                                <th scope="col" className="inCart">In Cart</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Facemask</td>
-                                <td>0 <br></br><button onClick={() => this.increaseQuantity()}>Add</button></td>
-                                <td>2.50</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Hand Sanitizer</td>
-                                <td>0 <br></br><button>Add</button></td>
-                                <td>2.99</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Box of bandages</td>
-                                <td>0 <br></br><button>Add</button></td>
-                                <td>5.00</td>
-                                <td>0</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+            <div className="table">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Item</th>
+                            <th scope="col" className="quantity">Quantity</th>
+                            <th scope="col" className="price">Price</th>
+                            <th scope="col" className="inCart">In Cart</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Facemask</td>
+                            <td>0 <br></br><button onClick={() => this.increaseQuantity()}>Add</button></td>
+                            <td>2.50</td>
+                            <td>0</td>
+                            <td><button onClick={() => this.printInfo()}>Print</button></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Hand Sanitizer</td>
+                            <td>0 <br></br><button>Add</button></td>
+                            <td>2.99</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Box of bandages</td>
+                            <td>0 <br></br><button>Add</button></td>
+                            <td>5.00</td>
+                            <td>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
--- sh-app/src/index.js
+++ sh-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Table from './components/Table'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
